0a108cc49a997159006d4e1d0ef21665b45c8bba
Kankuu <kankuu@vanders-MacBook-Pro.local>
2019-03-06 16:18:56 +0700

    initial commit

---
+++ helper.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+set -e
+
+# function
+curl-checker () {
+  if [ $(which curl > /dev/null 2>&1; echo $?) -gt 0 ]; then
+    echo -e "Error : package curl needed, try to install curl package."
+    exit
+  else
+    echo -e "[*] curl detected!"
+  fi
+}
+
+web-checker () {
+  domain=$1
+  echo "------------------------------------------------------------------"
+  if [ -z $domain ]; then
+    echo -e "./check [domain]"
+  else
+    echo -e "[*] CHECKING DOMAIN [$domain]"
+    alive-check $domain
+    if [ $(
+        curl -s $domain | grep -e hack -e Hack -e hAck -e haCk -e \
+        hacK -e HAck -e HaCk -e HacK -e HaCK -e hACk -e hAcK -e \
+        haCK -e h4ck -e H4ck -e h4ck -e h4Ck -e h4cK -e H4ck -e \
+        H4Ck -e H4cK -e H4CK -e h4Ck -e h4cK -e h4CK > /dev/null \
+        2>&1 ; echo $?) -eq 0 ]
+      then
+      echo -e "[*] website $domain is - [HACKED]"
+    else
+      echo -e "[*] website $domain is - [SAFE]"
+    fi
+  fi
+  echo "------------------------------------------------------------------"
+}
+
+alive-check () {
+  domain=$1
+  status=$(ping -t 1 $domain > /dev/null 2>&1; echo $?)
+  if [ $status -gt 0 ]; then
+    echo "--"
+    echo -e "[*] $domain is - [DOWN]"
+    echo "--"
+  else
+    echo -e "[*] server is - [UP]"
+  fi
+}
+
+# --------
+function ProgressBar {
+  # Process data
+      let _progress=(${1}*100/${2}*100)/100
+      let _done=(${_progress}*4)/10
+      let _left=40-$_done
+  # Build progressbar string lengths
+      _fill=$(printf "%${_done}s")
+      _empty=$(printf "%${_left}s")
+
+  # 1.2 Build progressbar strings and print the ProgressBar line
+  # 1.2.1 Output example:                           
+  # 1.2.1.1 Progress : [########################################] 100%
+  printf "\rProgress : [${_fill// /\#}${_empty// /-}] ${_progress}%%"
+}
+# --------
+
+hacked-check () {
+  curl-checker
+  list=$(cat $1)
+  mX=($(cat $1))
+  list_length=${#mX[@]}
+  idx=1
+  echo -e "--#PROGRESS:[${#list_length[@]}]-Domain:SCANNING..."
+  for i in $list; do
+  # for i in $(seq ${} ${_end}); do
+    web-checker $i >> web.log
+    echo -n "#"
+    ((idx++))
+  done
+  echo ""
+  echo -e "[*] SCANNING COMPLETE!"
+  echo ""
+}
+
+reset-weblog () {
+  echo -e "---reseting web.log"
+  echo "" > web.log
+  echo -e "[*] reseting [COMPLETE]"
+}
+
+list-hacked () {
+  echo -e "-----LIST WEBSITE IDENTIFY AS HACKED"
+  hacked=$(cat web.log | grep HACKED)
+  echo $hacked
+}
+
+list-down () {
+  echo -e "-----LIST WEBSITE IS DOWN"
+  down=$(cat web.log | grep DOWN)
+  echo $down
+}
---
+++ main.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+source helper.sh
+
+echo -e ""
+echo -e "..::[ welcome2 website checker ]::.."
+echo ""
+echo -e "--option:"
+echo -e "1. scan website"
+echo -e "2. list hacked website"
+echo -e "3. list down website"
+echo -e "4. reset log"
+echo -e "q. quit"
+echo -e "--ctrl+c to quit~"
+echo ""
+echo -ne "choise#::> "; read choise
+main () {
+  choise=$1
+  if [ -z $choise ]; then choise=9; fi
+  case $choise in
+  "1") hacked-check list.txt ;;
+  "2") list-hacked ;;
+  "3") list-down ;;
+  "4") reset-weblog ;;
+  "q" | "Q") 
+    echo "tanks for using :> "
+    echo "../kankuu\.."
+    exit 
+    ;;
+  *)
+    echo ""
+    echo -e "choise right answer!"
+    echo ""
+    ;;
+  esac
+  echo -ne "-----!!hoise#::_> "
+  read choiseN; main $choiseN
+}
+
+main $choise
