03b95362e56ddfd72b3bf6b79f273a8310e6b34d
abas <akhmadbasir5@gmail.com>
2019-03-08 22:28:57 +0700

    [UPDATE] change structure

--- wsc.sh
+++ wsc.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 set -e
 source helper.sh
+if [ -z $1 ]; then
+  
+else
+  echo -e ""
+  echo -e "..::[ welcome2 website checker ]::.."
+  echo ""
+  echo -e "--option:"
+  echo -e "1. scan website"
+  echo -e "2. list hacked website"
+  echo -e "3. list down website"
+  echo -e "4. reset log"
+  echo -e "q. quit"
+  echo -e "--ctrl+c to quit~"
+  echo ""
+  echo -ne "choise#::> "; read choise
+  main () {
+    choise=$1
+    if [ -z $choise ]; then choise=9; fi
+    case $choise in
+    "1") hacked-check list.txt ;;
+    "2") list-hacked ;;
+    "3") list-down ;;
+    "4") reset-weblog ;;
+    "q" | "Q") 
+      echo "tanks for using :> "
+      echo "../kankuu\.."
+      exit 
+      ;;
+    *)
+      echo ""
+      echo -e "choise right answer!"
+      echo ""
+      ;;
+    esac
+    echo -ne "-----!!choise#::_> "
+    read choiseN; main $choiseN
+  }
 
-echo -e ""
-echo -e "..::[ welcome2 website checker ]::.."
-echo ""
-echo -e "--option:"
-echo -e "1. scan website"
-echo -e "2. list hacked website"
-echo -e "3. list down website"
-echo -e "4. reset log"
-echo -e "q. quit"
-echo -e "--ctrl+c to quit~"
-echo ""
-echo -ne "choise#::> "; read choise
-main () {
-  choise=$1
-  if [ -z $choise ]; then choise=9; fi
-  case $choise in
-  "1") hacked-check list.txt ;;
-  "2") list-hacked ;;
-  "3") list-down ;;
-  "4") reset-weblog ;;
-  "q" | "Q") 
-    echo "tanks for using :> "
-    echo "../kankuu\.."
-    exit 
-    ;;
-  *)
-    echo ""
-    echo -e "choise right answer!"
-    echo ""
-    ;;
-  esac
-  echo -ne "-----!!choise#::_> "
-  read choiseN; main $choiseN
-}
-
-main $choise
+  main $choise
+fi
